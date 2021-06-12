0b606ea2a2c7c213ceafbe67986ccb33b5136880
Gustavo Kazuya Oshiro <gustavo@vizir.com.br>
2021-06-12 18:31:36 -0300

    add scenes back

--- client/game.ts
+++ client/game.ts
@@ -2,14 +2,14 @@ import { AUTO, Game, Scale } from "phaser";
 import { GameOver } from "./scenes/game-over";
 import { GameWorld } from "./scenes/game-world";
 
-new Game({
+export default new Game({
   type: AUTO,
   backgroundColor: "#125555",
   scale: {
     mode: Scale.FIT,
     autoCenter: Scale.CENTER_BOTH,
   },
-  scene: [],
+  scene: [GameWorld, GameOver],
   physics: {
     default: "arcade",
     arcade: {
