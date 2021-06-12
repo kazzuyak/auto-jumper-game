c9d8aeb33364160191e195f04b45fd2d2a8d25cf
Gustavo Kazuya Oshiro <gustavo@vizir.com.br>
2021-06-12 17:55:02 -0300

    debugging: temporally remove bundling

--- server/server.ts
+++ server/server.ts
@@ -4,7 +4,7 @@ import Bundler from "parcel-bundler";
 const app = express();
 
 const clientPath = __dirname + "/../client/game.html";
-const bundler = new Bundler(clientPath, {});
-app.use(bundler.middleware());
+// const bundler = new Bundler(clientPath, {});
+// app.use(bundler.middleware());
 
 app.listen(process.env.PORT || 3000);
