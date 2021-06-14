ec2bb525683b02fd65c2ab7e7ed8971b9042019e
Gustavo Kazuya Oshiro <gustavo@vizir.com.br>
2021-06-14 08:08:48 -0300

    move jump logic to the jumper

--- client/entities/jumper.ts
+++ client/entities/jumper.ts
@@ -1,11 +1,130 @@
-import { GameObjects, Physics, Scene } from "phaser";
+import { GameObjects, Input, Physics, Scene } from "phaser";
 import { ImageEnum } from "../enums/ImageEnum";
-
+interface ICursors {
+  left: Input.Keyboard.Key;
+  right: Input.Keyboard.Key;
+  A: Input.Keyboard.Key;
+  D: Input.Keyboard.Key;
+}
 export class Jumper extends GameObjects.Sprite {
   public body!: Physics.Arcade.Body;
+  private cursors!: ICursors;
 
   constructor(scene: Scene, x: number, y: number) {
     super(scene, x, y, ImageEnum.JumperJumping);
     this.scale = 0.5;
+
+    scene.physics.add.existing(this);
+    scene.add.existing(this);
+    this.body.checkCollision.left = false;
+    this.body.checkCollision.right = false;
+    this.body.checkCollision.up = false;
+
+    this.cursors = this.scene.input.keyboard.addKeys(
+      "left,right,A,D",
+    ) as ICursors;
+  }
+
+  public static preLoad(scene: Scene) {
+    scene.load.image(
+      ImageEnum.JumperJumping,
+      require("../assets/bunny1_jump.png"),
+    );
+    scene.load.image(
+      ImageEnum.JumperStanding,
+      require("../assets/bunny1_ready.png"),
+    );
+    scene.load.image(
+      ImageEnum.JumperWalking,
+      require("../assets/bunny1_walk1.png"),
+    );
+  }
+
+  public preUpdate() {
+    this.jumpIfTouchingDown();
+    this.changeTextureIfFalling();
+    this.horizontalWrap();
+    this.handlePlayerInput();
+  }
+
+  private jumpIfTouchingDown() {
+    if (this.body.touching.down) {
+      this.body.setVelocityY(-this.scene.scale.height * 0.7);
+      this.setTexture(ImageEnum.JumperJumping);
+    }
+  }
+
+  private changeTextureIfFalling() {
+    if (
+      this.body.velocity.y > 0 &&
+      this.texture.key !== ImageEnum.JumperStanding
+    ) {
+      this.setTexture(ImageEnum.JumperStanding);
+    }
+  }
+
+  private horizontalWrap() {
+    const halfWidth = this.displayWidth * 0.5;
+    const gameWidth = this.scene.scale.width;
+    if (this.x < -halfWidth) {
+      this.x = gameWidth + halfWidth;
+    } else if (this.x > gameWidth + halfWidth) {
+      this.x = -halfWidth;
+    }
+  }
+
+  private handlePlayerInput() {
+    const touchingDown = this.body.touching.down;
+
+    const playerInput = this.getPlayerInput();
+
+    let playerVelocity = 0;
+
+    if (playerInput.left && !touchingDown) {
+      playerVelocity -= this.scene.scale.width * 0.34;
+    }
+
+    if (playerInput.right && !touchingDown) {
+      playerVelocity += this.scene.scale.width * 0.34;
+    }
+
+    if (playerVelocity !== 0) {
+      const fliX = playerVelocity > 0 ? false : true;
+
+      this.setTexture(ImageEnum.JumperWalking).setFlipX(fliX);
+    }
+
+    this.body.setVelocityX(playerVelocity);
+  }
+
+  private getPlayerInput(): { left: boolean; right: boolean } {
+    let isPressingLeft = false;
+    let isPressingRight = false;
+
+    const pointer1 = this.scene.input.pointer1;
+    const pointer2 = this.scene.input.pointer2;
+
+    if (
+      (pointer1.isDown && pointer1.x >= this.scene.scale.width / 2) ||
+      (pointer2.isDown && pointer2.x >= this.scene.scale.width / 2) ||
+      this.cursors.right.isDown ||
+      this.cursors.D.isDown
+    ) {
+      isPressingRight = true;
+    }
+
+    if (
+      (pointer1.isDown && pointer1.x < this.scene.scale.width / 2) ||
+      (pointer2.isDown && pointer2.x < this.scene.scale.width / 2) ||
+      this.cursors.left.isDown ||
+      this.cursors.A.isDown
+    ) {
+      isPressingLeft = true;
+    }
+
+    return {
+      left: isPressingLeft,
+      right: isPressingRight,
+    };
   }
 }
--- client/scenes/game-world.ts
+++ client/scenes/game-world.ts
@@ -1,20 +1,11 @@
-import { Input, Physics, Scene } from "phaser";
+import { Physics, Scene } from "phaser";
 import { Jumper } from "../entities/jumper";
 import { Platform } from "../entities/platform";
 import { ScoreCounter } from "../entities/score-counter";
-import { ImageEnum } from "../enums/ImageEnum";
-
-interface ICursors {
-  left: Input.Keyboard.Key;
-  right: Input.Keyboard.Key;
-  A: Input.Keyboard.Key;
-  D: Input.Keyboard.Key;
-}
 
 export class GameWorld extends Scene {
   private platforms!: Physics.Arcade.StaticGroup;
   private player!: Jumper;
-  private cursors!: ICursors;
   private scoreCounter!: ScoreCounter;
 
   constructor() {
@@ -22,18 +13,7 @@ export class GameWorld extends Scene {
   }
 
   public preload() {
-    this.load.image(
-      ImageEnum.JumperJumping,
-      require("../assets/bunny1_jump.png"),
-    );
-    this.load.image(
-      ImageEnum.JumperStanding,
-      require("../assets/bunny1_ready.png"),
-    );
-    this.load.image(
-      ImageEnum.JumperWalking,
-      require("../assets/bunny1_walk1.png"),
-    );
+    Jumper.preLoad(this);
   }
 
   public create() {
@@ -65,14 +45,7 @@ export class GameWorld extends Scene {
       firstPlatform.position.y - firstPlatform.height - this.scale.height * 0.1,
     );
 
-    this.physics.add.existing(this.player);
-    this.add.existing(this.player);
-
     this.physics.add.collider(this.platforms, this.player);
-    this.player.body.checkCollision.left = false;
-    this.player.body.checkCollision.right = false;
-    this.player.body.checkCollision.up = false;
-    this.cursors = this.input.keyboard.addKeys("left,right,A,D") as ICursors;
 
     this.cameras.main.startFollow(this.player, false, 0, 1);
     this.player.x = firstPlatform.position.x + firstPlatform.halfWidth;
@@ -101,42 +74,6 @@ export class GameWorld extends Scene {
       }
     });
 
-    const touchingDown = this.player.body.touching.down;
-
-    if (touchingDown) {
-      this.player.body.setVelocityY(-this.scale.height * 0.7);
-      this.player.setTexture(ImageEnum.JumperJumping);
-    }
-
-    if (
-      this.player.body.velocity.y > 0 &&
-      this.player.texture.key !== ImageEnum.JumperStanding
-    ) {
-      this.player.setTexture(ImageEnum.JumperStanding);
-    }
-
-    const playerInput = this.getPlayerInput();
-
-    let playerVelocity = 0;
-
-    if (playerInput.left && !touchingDown) {
-      playerVelocity -= this.scale.width * 0.34;
-    }
-
-    if (playerInput.right && !touchingDown) {
-      playerVelocity += this.scale.width * 0.34;
-    }
-
-    if (playerVelocity !== 0) {
-      const fliX = playerVelocity > 0 ? false : true;
-
-      this.player.setTexture(ImageEnum.JumperWalking).setFlipX(fliX);
-    }
-
-    this.player.body.setVelocityX(playerVelocity);
-
-    this.horizontalWrap(this.player);
-
     const bottomPlatform = this.findBottomMostPlatform();
     if (
       this.player.y >
@@ -144,37 +81,6 @@ export class GameWorld extends Scene {
     ) {
       this.scene.start("game-over", { score: this.scoreCounter.score });
     }
-  }
-
-  private getPlayerInput(): { left: boolean; right: boolean } {
-    let isPressingLeft = false;
-    let isPressingRight = false;
-
-    const pointer1 = this.input.pointer1;
-    const pointer2 = this.input.pointer2;
-
-    if (
-      (pointer1.isDown && pointer1.x >= this.scale.width / 2) ||
-      (pointer2.isDown && pointer2.x >= this.scale.width / 2) ||
-      this.cursors.right.isDown ||
-      this.cursors.D.isDown
-    ) {
-      isPressingRight = true;
-    }
-
-    if (
-      (pointer1.isDown && pointer1.x < this.scale.width / 2) ||
-      (pointer2.isDown && pointer2.x < this.scale.width / 2) ||
-      this.cursors.left.isDown ||
-      this.cursors.A.isDown
-    ) {
-      isPressingLeft = true;
-    }
-
-    return {
-      left: isPressingLeft,
-      right: isPressingRight,
-    };
   }
 
   private findBottomMostPlatform() {
@@ -192,16 +98,6 @@ export class GameWorld extends Scene {
     }
 
     return bottomPlatform;
-  }
-
-  private horizontalWrap(player: Jumper) {
-    const halfWidth = player.displayWidth * 0.5;
-    const gameWidth = this.scale.width;
-    if (player.x < -halfWidth) {
-      player.x = gameWidth + halfWidth;
-    } else if (player.x > gameWidth + halfWidth) {
-      player.x = -halfWidth;
-    }
   }
 
   private getRandomPlatformX(platformWidth: number) {
